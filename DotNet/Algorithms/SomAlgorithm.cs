a9a2cdafda21bf3ab340c06c9e3c8904e8d0daf6
kimjo8317 <kimjo8317@naver.com>
2023-09-11 00:40:56 +0900

    SumAlgorithm

--- DotNet/Algorithms/SomAlgorithm.cs
+++ DotNet/Algorithms/SomAlgorithm.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DotNet.Algorithms
+
+class SumAlgorithm
 {
-    internal class SomAlgorithm
+    static void Main()
     {
+        //[1] Input = ex) 데이터구조, 자료구조 영역
+        //[2] Process = 알고리즘 영역
+        //[3] Output = ex) C#콘솔 ,윈도우폼, 웹폼 등등
+
+        //문제1 n명의 영어중에서 80점이상인 점수의 합계 구하기 
+        int[] scores = { 100, 75, 50, 37, 90, 95 };
+        int sum = 0;
+
+        for (int i = 0; i < scores.Length; i++)
+        {
+            if (scores[i] >= 80)
+            {
+                sum += scores[i];
+            }
+        }
+
+        Console.WriteLine($"{scores.Length}명의 점수중 80점 이상의 총점(합계):{sum}");
     }
 }
